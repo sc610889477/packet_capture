c21ecc080ccb3b9839c9e8f56bd0f5552f555049
SongChen <songchen@songchens-MacBook-Pro.local>
2022-02-21 20:48:59 +0900

    add capture function

--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 これはプログラミング言語Rustを用いて，TCP/IP通信パケットをキャプチャするプラグラムである．
 
 ## Usage
-### An example of get an interface
+### An example of interface
 ~~~
 $ cargo run en0
     Finished dev [unoptimized + debuginfo] target(s) in 0.01s
     Running `target/debug/capture en0`
-Selected Interface Name: en0
+[en0]: TCP Packet: 10.70.70.xxx:xxx > 10.70.70.xxx:xxx; length: 142
+[en0]: TCP Packet: 10.70.70.xxx:xxx > 10.70.70.xxx:xxx; length: 32
+[en0]: TCP Packet: 10.70.70.xxx:xxx > xxxxxxxxxxxx:443; length: 87
+[en0]: TCP Packet: xxxxxxxxxxxx:xxx > 10.70.70.xxx:xxx; length: 32
+(略)
 $ 
 ~~~
 or
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,224 @@
 extern crate pnet;
 use pnet::datalink;
+use pnet::datalink::{NetworkInterface};
+use pnet::packet::arp::ArpPacket;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::icmp::{echo_reply, echo_request, IcmpPacket, IcmpTypes};
+use pnet::packet::ip::{IpNextHeaderProtocol, IpNextHeaderProtocols};
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::udp::UdpPacket;
+use pnet::packet::Packet;
+
+use std::io::Write;
+use std::net::IpAddr;
+
+fn handle_udp_packet(interface_name: &str, source: IpAddr, destination: IpAddr, packet: &[u8]) {
+    let udp = UdpPacket::new(packet);
+
+    if let Some(udp) = udp {
+        println!(
+            "[{}]: UDP Packet: {}:{} > {}:{}; length: {}",
+            interface_name,
+            source,
+            udp.get_source(),
+            destination,
+            udp.get_destination(),
+            udp.get_length()
+        );
+    } else {
+        println!("[{}]: Malformed UDP Packet", interface_name);
+    }
+}
+
+fn handle_icmp_packet(interface_name: &str, source: IpAddr, destination: IpAddr, packet: &[u8]) {
+    let icmp_packet = IcmpPacket::new(packet);
+    if let Some(icmp_packet) = icmp_packet {
+        match icmp_packet.get_icmp_type() {
+            IcmpTypes::EchoReply => {
+                let echo_reply_packet = echo_reply::EchoReplyPacket::new(packet).unwrap();
+                println!(
+                    "[{}]: ICMP echo reply {} -> {} (seq={:?}, id={:?})",
+                    interface_name,
+                    source,
+                    destination,
+                    echo_reply_packet.get_sequence_number(),
+                    echo_reply_packet.get_identifier()
+                );
+            }
+            IcmpTypes::EchoRequest => {
+                let echo_request_packet = echo_request::EchoRequestPacket::new(packet).unwrap();
+                println!(
+                    "[{}]: ICMP echo request {} -> {} (seq={:?}, id={:?})",
+                    interface_name,
+                    source,
+                    destination,
+                    echo_request_packet.get_sequence_number(),
+                    echo_request_packet.get_identifier()
+                );
+            }
+            _ => println!(
+                "[{}]: ICMP packet {} -> {} (type={:?})",
+                interface_name,
+                source,
+                destination,
+                icmp_packet.get_icmp_type()
+            ),
+        }
+    } else {
+        println!("[{}]: Malformed ICMP Packet", interface_name);
+    }
+}
+
+fn handle_tcp_packet(interface_name: &str, source: IpAddr, destination: IpAddr, packet: &[u8]) {
+    let tcp = TcpPacket::new(packet);
+    if let Some(tcp) = tcp {
+        println!(
+            "[{}]: TCP Packet: {}:{} > {}:{}; length: {}",
+            interface_name,
+            source,
+            tcp.get_source(),
+            destination,
+            tcp.get_destination(),
+            packet.len()
+        );
+    } else {
+        println!("[{}]: Malformed TCP Packet", interface_name);
+    }
+}
+
+fn handle_transport_protocol(
+    interface_name: &str,
+    source: IpAddr,
+    destination: IpAddr,
+    protocol: IpNextHeaderProtocol,
+    packet: &[u8],
+) {
+    match protocol {
+        IpNextHeaderProtocols::Udp => {
+            handle_udp_packet(interface_name, source, destination, packet)
+        }
+        IpNextHeaderProtocols::Tcp => {
+            handle_tcp_packet(interface_name, source, destination, packet)
+        }
+        IpNextHeaderProtocols::Icmp => {
+            handle_icmp_packet(interface_name, source, destination, packet)
+        }
+        _ => println!(
+            "[{}]: Unknown IPv4 packet: {} > {}; protocol: {:?} length: {}",
+            interface_name,
+            source,
+            destination,
+            protocol,
+            packet.len()
+        ),
+    }
+}
+
+fn handle_ipv4_packet(interface_name: &str, ethernet: &EthernetPacket) {
+    let header = Ipv4Packet::new(ethernet.payload());
+    if let Some(header) = header {
+        handle_transport_protocol(
+            interface_name,
+            IpAddr::V4(header.get_source()),
+            IpAddr::V4(header.get_destination()),
+            header.get_next_level_protocol(),
+            header.payload(),
+        );
+    } else {
+        println!("[{}]: Malformed IPv4 Packet", interface_name);
+    }
+}
+
+fn handle_arp_packet(interface_name: &str, ethernet: &EthernetPacket) {
+    let header = ArpPacket::new(ethernet.payload());
+    if let Some(header) = header {
+        println!(
+            "[{}]: ARP packet: {}({}) > {}({}); operation: {:?}",
+            interface_name,
+            ethernet.get_source(),
+            header.get_sender_proto_addr(),
+            ethernet.get_destination(),
+            header.get_target_proto_addr(),
+            header.get_operation()
+        );
+    } else {
+        println!("[{}]: Malformed ARP Packet", interface_name);
+    }
+}
+
+fn handle_ethernet_frame(interface: &NetworkInterface, ethernet: &EthernetPacket) {
+    let interface_name = &interface.name;
+    match ethernet.get_ethertype() {
+        // IPv6未実装
+        EtherTypes::Ipv4 => handle_ipv4_packet(interface_name, ethernet),
+        EtherTypes::Arp => handle_arp_packet(interface_name, ethernet),
+        _ => println!(
+            "[{}]: Unknown packet: {} > {}; ethertype: {:?} length: {}",
+            interface_name,
+            ethernet.get_source(),
+            ethernet.get_destination(),
+            ethernet.get_ethertype(),
+            ethernet.packet().len()
+        )
+    }
+}
 
 fn main() {
-    // キャプチャに使うインターフェース名を1番目のコマンド引数から取得する．0番目はプログラムの名前
-    // unwrapはエラーメッセージの出力が成功したかどうかをチェックする．失敗した場合はエラーメッセージを出力する
-    let arg = std::env::args().nth(1).unwrap();
+    // キャプチャに使うインターフェース名をコマンド引数の1番目から取得する．0番目はプログラムの名前
+    let arg = match std::env::args().len() {
+        2 => std::env::args().nth(1).unwrap(),
+        _ => {
+            writeln!(std::io::stderr(), "Usage: capture <NETWORK INTERFACE>").unwrap();
+            std::process::exit(1);
+        }
+    };
 
     // すべてのネットワークインターフェースを取得する
     let interfaces = datalink::interfaces();
 
     // 使いたいインターフェースが存在すれば値はOption<T>型として取得し，存在しない場合はNoneで返す
-    let interface = interfaces.iter().find(|iface| iface.name == arg);
-    match interface {
+    let interface = match interfaces.iter().find(|iface| iface.name == arg){
         None => {
-            println!("Failed get Interface!");
+            writeln!(std::io::stderr(), "Failed to get NETWORK INTERFACE!").unwrap();
             std::process::exit(1);
-        },
-        Some(interface) => Some(interface)
+        }
+        // Some(interface) => Some(interface)
+        Some(interface) => interface
     };
-    println!("Selected Interface Name: {}", interface.unwrap().name);
+    // println!("Selected Interface Name: {}", interface.name);
+
+    // 受信用のチャネルを作成する
+    let config: datalink::Config = Default::default();
+    // チャネルの配置状況
+    // Struct Config{
+        // write_buffer_size: 4096, 
+        // read_buffer_size: 4096, 
+        // read_timeout: None, 
+        // write_timeout: None, 
+        // channel_type: Layer2, 
+        // bpf_fd_attempts: 1000, 
+        // linux_fanout: None, 
+        // promiscuous: true 
+    // }
+    // println!("{:?}", config);
+
+    let (_tx, mut rx) = match datalink::channel(interface, config) {
+        Ok(datalink::Channel::Ethernet(tx, rx)) => {
+            // println!("Create channel successful.");
+            (tx, rx)
+        }
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => panic!("Unable to create channel: {}", e)
+    };
+
+    loop {
+        match rx.next() {
+            Ok(packet) => {
+                let ethernet_frame = EthernetPacket::new(packet).unwrap();
+                handle_ethernet_frame(interface, &ethernet_frame);
+            }
+            Err(e) => panic!("Unable to receive packet: {}", e)
+        }
+    }
 }
