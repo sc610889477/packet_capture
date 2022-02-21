c07006a7fabe54a554b448fa24dc746acd867f0c
SongChen <songchen@songchens-MacBook-Pro.local>
2022-02-21 16:21:39 +0900

    get interface

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # packet_capture
 これはプログラミング言語Rustを用いて，TCP/IP通信パケットをキャプチャするプラグラムである．
+
+## Usage
+### An example of get an interface
+~~~
+$ cargo run en0
+    Finished dev [unoptimized + debuginfo] target(s) in 0.01s
+    Running `target/debug/capture en0`
+Selected Interface Name: en0
+$ 
+~~~
+or
+~~~
+$ cargo run 00
+    Finished dev [unoptimized + debuginfo] target(s) in 0.01s
+    Running `target/debug/capture 00`
+Failed get Interface!
+$ 
+~~~
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate pnet;
+use pnet::datalink;
+
 fn main() {
-    println!("Hello, world!");
+    // キャプチャに使うインターフェース名を1番目のコマンド引数から取得する．0番目はプログラムの名前
+    // unwrapはエラーメッセージの出力が成功したかどうかをチェックする．失敗した場合はエラーメッセージを出力する
+    let arg = std::env::args().nth(1).unwrap();
+
+    // すべてのネットワークインターフェースを取得する
+    let interfaces = datalink::interfaces();
+
+    // 使いたいインターフェースが存在すれば値はOption<T>型として取得し，存在しない場合はNoneで返す
+    let interface = interfaces.iter().find(|iface| iface.name == arg);
+    match interface {
+        None => {
+            println!("Failed get Interface!");
+            std::process::exit(1);
+        },
+        Some(interface) => Some(interface)
+    };
+    println!("Selected Interface Name: {}", interface.unwrap().name);
 }
